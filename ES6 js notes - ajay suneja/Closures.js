50e5ff2745d3a244e8cebbd836f492f2f28b8167
Apoorv <apoorvsharma494@gmail.com>
2022-09-20 02:46:23 +0530

    commit

--- ES6 js notes - ajay suneja/Closures.js
+++ ES6 js notes - ajay suneja/Closures.js
@@ -1,5 +1,7 @@
 // function without name ---> anonymous function 
 // what is closure ? values inside the function is not lost
+
+// example 1--------------- returning 1 function in form of object
 function sum(a) {
 
     console.log("Live Viewers " + a)
@@ -16,3 +18,22 @@ function sum(a) {
 
 let returnFunc = sum(2)
 console.log(returnFunc(3))
+
+// example 2--------------- returning 2 functions in form of object
+
+function sum1(a, b, c) {
+    return {
+        getSumTwo:function () {
+            return a*b
+        },
+        getSumThree:function () {
+            return a*b*c
+        }
+    }
+  
+}
+
+let store = sum1(2, 3, 4)
+// even after returning the function , the values of a b and c are retained and are used in function getSumTwo and getSumThree, this is lexical scoping and closure
+console.log(store.getSumTwo())
+console.log(store.getSumThree())
